2e7c9a1a5dfd24e0ad0a40cf094b3f5a2bf2e911
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-02-09 21:03:28 +0100

    Creaed first screen and passed dummy data

--- app/src/main/java/com/ppam/myapplication/MainActivity.kt
+++ app/src/main/java/com/ppam/myapplication/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.ppam.myapplication.data.DummyData
+import com.ppam.myapplication.screens.LeaderListScreen
 import com.ppam.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +25,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//                    Greeting("Android")
+//                    LeaderListScreen(leaders = , onLeaderSelected = )
+                    LeaderListScreen(leaders = DummyData.getDummyLeaders(), onItemClick = {
+                        leader ->
+                        // Handle detail screen
+                    })
                 }
             }
         }
--- app/src/main/java/com/ppam/myapplication/screens/LeaderListScreen.kt
+++ app/src/main/java/com/ppam/myapplication/screens/LeaderListScreen.kt
@@ -1,75 +1,103 @@
 package com.ppam.myapplication.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.ppam.myapplication.R
 import com.ppam.myapplication.data.DummyData
 import com.ppam.myapplication.data.Leader
 import com.ppam.myapplication.ui.theme.MyApplicationTheme
 
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LeaderListScreen(
-    leaders: List<Leader>,
-    onLeaderSelected: (Leader) -> Unit
-) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
+fun LeaderListScreen(leaders: List<Leader>, onItemClick: (Leader) -> Unit) {
+
+    val appBarHeight = dimensionResource(id = R.dimen.top_app_bar_height)
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Prime Minister of India") }
+            )
+        }
     ) {
-        Text(
-            text = "List of Leaders",
-            fontSize = 24.sp,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-        LazyColumn {
+
+        LazyColumn(
+            contentPadding = PaddingValues(top = appBarHeight)
+        ) {
             items(leaders) { leader ->
-                LeaderListItem(leader = leader, onClick = { onLeaderSelected(leader) })
+                LeaderCard(leader = leader, onItemClick = onItemClick)
+            }
+        }
+    }
+}
+@Composable
+fun LeaderCard(leader: Leader, onItemClick: (Leader) -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable { onItemClick(leader) },
+        elevation = CardDefaults.cardElevation(
+                defaultElevation = 10.dp
+                )
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Display the leader image here
+            Image(
+                painter = painterResource(id = R.drawable.baseline_person_2_24), // Assuming you have image resources
+                contentDescription = null, // Provide proper content description
+                modifier = Modifier.size(72.dp)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Column {
+                Text(text = leader.name, style = MaterialTheme.typography.bodyLarge)
+                Text(text = leader.party, style = MaterialTheme.typography.bodySmall)
             }
         }
     }
 }
 
-@Composable
-fun LeaderListItem(
-    leader: Leader,
-    onClick: () -> Unit
-) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 8.dp)
-            .clickable(onClick = onClick),
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 4.dp
-        )
-    ) {
-        Text(
-            text = leader.name,
-            fontSize = 18.sp,
-            modifier = Modifier.padding(16.dp)
-        )
-    }
-}
-
-@Preview
-@Composable
-fun PreviewLeaderListScreen() {
-    val leaders = DummyData.getDummyLeaders()
-
-    MyApplicationTheme {
-        LeaderListScreen(leaders = leaders, onLeaderSelected = {})
-    }
-}
+//@Preview
+//@Composable
+//fun PreviewLeaderListScreen() {
+//    val leaders = DummyData.getDummyLeaders()
+//
+//    MyApplicationTheme {
+//        LeaderListScreen(leaders = leaders, onLeaderSelected = {})
+//    }
+//}
